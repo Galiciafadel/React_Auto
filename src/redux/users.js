8b279a8bfd3207323c8a00df6e901f748a443fec
Galicia Fadel <galicia.fadel@hotmail.com>
2021-04-27 17:40:36 +0300

    fixing users.js

--- src/redux/users.js
+++ src/redux/users.js
@@ -1,16 +1,16 @@
 import * as ActionTypes from './ActionTypes';
 
-export const RoomType = (state = { isLoading: true,
+export const Users = (state = { isLoading: true,
     errMess: null,
-    roomType:[]}, action) => {
+    users:[]}, action) => {
     switch (action.type) {
-        case ActionTypes.ADD_ROOMTYPE:
-            return {...state, isLoading: false, errMess: null, roomType: action.payload};
+        case ActionTypes.ADD_USERS:
+            return {...state, isLoading: false, errMess: null, users: action.payload};
 
-        case ActionTypes.ROOMTYPE_LOADING:
-            return {...state, isLoading: true, errMess: null, roomType: []}
+        case ActionTypes.USERS_LOADING:
+            return {...state, isLoading: true, errMess: null, users: []}
 
-        case ActionTypes.ROOMTYPE_FAILED:
+        case ActionTypes.USERS_FAILED:
             return {...state, isLoading: false, errMess: action.payload};
 
         default:
